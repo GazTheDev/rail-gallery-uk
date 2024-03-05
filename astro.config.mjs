209e4827fd2bcaa2675e21c0306012299a59e767
Gareth Davies <daviesg77@gmail.com>
2024-03-05 14:04:07 +0000

    New changes and new version

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,12 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
-
 import netlify from '@astrojs/netlify/functions';
+
+import auth from "auth-astro";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()],
+  integrations: [tailwind(), auth()],
   output: 'server',
   adapter: netlify()
 });
---
+++ src/content/blog/obb-vorelberg.md
@@ -0,0 +1,16 @@
+---
+title: ' TSW 4: S-BAHN VORARLBERG - OBB 4024 EMU'
+pubDate: 2024-03-05
+author: 'Gareth Davies'
+image: 'obb.jpg'
+tags: ['EMU', 'Austria', 'OBB 4024']
+slug: S-BAHN-VORARLBERG-OBB-4024-EMU
+unit: OBB 4024
+---
+
+Some TSW 4 content today! 
+
+OBB 4024 from the S-BAHN Vorarleberg route for TSW4. 
+
+Lovely route! 
+
