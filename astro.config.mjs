e8ff13ebea154048bc82c738c4d8b7513bce67e1
Gareth Davies <daviesg77@gmail.com>
2024-03-05 14:09:55 +0000

    Auth taken away

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,6 @@ import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import netlify from '@astrojs/netlify/functions';
 
-import auth from "auth-astro";
 
 // https://astro.build/config
 export default defineConfig({
